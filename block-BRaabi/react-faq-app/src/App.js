82ab29468c8d3a8223d24aae74821a3df91a907e
Anand Seshadri <78548214+anandsesha@users.noreply.github.com>
2024-03-16 23:17:36 -0400

    react FAQ completed

--- block-BRaabi/react-faq-app/src/App.js
+++ block-BRaabi/react-faq-app/src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import Data from './data.json';
-import Answer from './Answer';
+// import Answer from './Answer';
 class App extends React.Component {
   // eslint-disable-next-line no-useless-constructor
   constructor(props) {
     super(props);
     this.state = {
-      window: 'closed',
+      // window: 'closed',
+      activeIndex: null,
     };
   }
-  toggleWindow = () => {
-    this.setState({
-      window: this.state.window === 'closed' ? 'open' : 'closed',
-    });
-  };
+  // toggleWindow = (i) => {
+  //   this.setState({
+  //     activeIndex: i,
+  //     window: this.state.window === 'closed' ? 'open' : 'closed',
+  //   });
+  // };
   render() {
     return (
       <center>
@@ -22,11 +24,22 @@ class App extends React.Component {
           <div key={obj.Q} className="faq-item">
             <div className="faq-question">
               <h5>{obj.Q}</h5>
-              <button onClick={this.toggleWindow}>⬇️</button>
+
+              <button
+                onClick={() =>
+                  this.setState({
+                    activeIndex: this.state.activeIndex === i ? null : i,
+                  })
+                }
+              >
+                {this.state.activeIndex === i ? '⬆️' : '⬇️'}
+              </button>
             </div>
-            <>
-              <Answer currentState={this.state.window} index={i} />
-            </>
+            {i === this.state.activeIndex ? (
+              <div className="answer--open">{obj.A}</div>
+            ) : (
+              ''
+            )}
           </div>
         ))}
       </center>
