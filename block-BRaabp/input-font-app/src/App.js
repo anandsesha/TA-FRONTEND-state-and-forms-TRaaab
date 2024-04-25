8e3381f1eb8d85f3b9aeb30ccabe91e304927bfe
Anand Seshadri <78548214+anandsesha@users.noreply.github.com>
2024-04-25 02:10:35 -0400

    State font app complete

--- block-BRaabp/input-font-app/src/App.js
+++ block-BRaabp/input-font-app/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputText: '',
+    };
+  }
+  handleChange = (event) => {
+    console.log(event);
+    this.setState(
+      {
+        inputText: event.target.value,
+      },
+      () => {
+        console.log(this.state.inputText);
+      }
+    );
+  };
+  render() {
+    return (
+      <center>
+        <h1>React INPUT - Font App</h1>
+        <label htmlFor="input">Enter the text here:</label>
+        <input
+          type="text"
+          id="input"
+          value={this.state.inputText}
+          onChange={this.handleChange}
+        />
+
+        <div className="container flex jsb aic flex-wrap">
+          {[
+            'Arial',
+            'Courier New',
+            'Times New Roman',
+            'Impact',
+            'Comic Sans MS',
+            'Verdana',
+            'Lucida Console',
+            'Tahoma',
+            'Georgia',
+            'Trebuchet MS',
+          ].map((font, i) => (
+            <div className="box">
+              <h5 className="font-type">{font}</h5>
+              <h2 style={{ fontFamily: font }}>{this.state.inputText}</h2>
+            </div>
+          ))}
+        </div>
+      </center>
+    );
+  }
 }
 
 export default App;
