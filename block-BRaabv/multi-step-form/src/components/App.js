233fc0c1ccec93741764a88f32b7b5fda2aa23a9
Anand Seshadri <78548214+anandsesha@users.noreply.github.com>
2024-09-17 00:09:01 -0400

    Mulit-Step Form using State

---
+++ block-BRaabv/multi-step-form/src/components/App.js
@@ -0,0 +1,11 @@
+// import './App.css';
+import MasterForm from './MasterForm';
+
+function App() {
+  return (
+    // eslint-disable-next-line react/jsx-no-undef
+    <MasterForm />
+  );
+}
+
+export default App;
